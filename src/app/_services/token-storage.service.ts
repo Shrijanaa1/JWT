218fac6d0fc26db637876ac7af288b3d89b335a9
Shrijanaa1 <srznakrki111@gmail.com>
2023-07-19 09:35:24 +0545

    first commit

---
+++ src/app/_services/token-storage.service.ts
@@ -0,0 +1,74 @@
+  // import { Injectable } from '@angular/core';
+
+  // const TOKEN_KEY = 'auth-token';
+  // const USER_KEY = 'auth-user';
+
+  // @Injectable({
+  //   providedIn: 'root'
+  // })
+  // export class TokenStorageService {
+
+  //   constructor() { }
+
+  //   signOut() {
+  //     window.sessionStorage.clear();
+  //   }
+
+  //   public saveToken(token: string) {
+  //     window.sessionStorage.removeItem(TOKEN_KEY);
+  //     window.sessionStorage.setItem(TOKEN_KEY, token);
+  //   }
+
+  //   public getToken(): string {
+  //     return sessionStorage.getItem(TOKEN_KEY);
+  //   }
+
+  //   public saveUser(user: any) {
+  //     window.sessionStorage.removeItem(USER_KEY);
+  //     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  //   }
+
+  //   public getUser() {
+  //     return JSON.parse(sessionStorage.getItem(USER_KEY));
+  //   }
+  // }
+
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+
+  constructor() { }
+
+  signOut() {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string | null) {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    if (token !== null) {
+      window.sessionStorage.setItem(TOKEN_KEY, token);
+    }
+  }
+
+  public getToken(): string | null {
+    return sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: any) {
+    window.sessionStorage.removeItem(USER_KEY);
+    if (user !== null) {
+      window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+  }
+
+  public getUser(): any {
+    const user = sessionStorage.getItem(USER_KEY);
+    return user !== null ? JSON.parse(user) : null;
+  }
+}
